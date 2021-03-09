df3fdffb9c30363c260120f67b4cfc843250013c
Megan Nyland <Megan Nyland@DESKTOP-2LAHT55>
2021-03-08 22:42:21 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace NylandCallingCinJavascript
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.printPreviewControl1 = new System.Windows.Forms.PrintPreviewControl();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // printPreviewControl1
+            // 
+            this.printPreviewControl1.Location = new System.Drawing.Point(484, 16);
+            this.printPreviewControl1.Name = "printPreviewControl1";
+            this.printPreviewControl1.Size = new System.Drawing.Size(293, 354);
+            this.printPreviewControl1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 16);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(449, 354);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 384);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.printPreviewControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PrintPreviewControl printPreviewControl1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace NylandCallingCinJavascript
+{
+    public partial class Form1 : Form
+    {
+        [ComVisible(true)]
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+            this webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+
+            //if you want to call the C# code (method in java script function then write this code
+            webBrowser1.ObjectForScripting = this;
+            webBrowser1.ScriptErrorsSuppressed = false;
+
+            //if you want to disable right click on web browser control then write this code
+            webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            webBrowser1.AllowWebBrowserDrop = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // in below written code you have to get the directory of this application
+            string CurrentDirectory = Directory.GetCurrentDirectory();
+            //Here you have to call HTML page using navigate method. It's mandatoryto call navigate method when you have to fire web browser Document Completed Event
+            webBrowser1.Navigate(Path.Combine(CurrentDirectory, "HTMLPageforJavaScript.html"));
+        }
+
+        private void Report()
+        {
+            //here I have to get the HTML page from if of Div
+            HtmlElement div = webBrowser1.Document.GetElementById("reportContent");
+
+            //here create a simple html content
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<table>");
+            sb.Append("<tr><td><B> Hi this is my report demo</B></td></tr>");
+            sb.Append("</table>");
+
+            //here I have to assign content to the HTML page div which is displayed on browser control
+            div.InnerHtml = sb.ToString();
+        }
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //when the form is load cursor focus on the Web Browser control
+            webBrowser1.Focus();
+
+            //call report method which contains the report content
+            Report();
+        }
+
+        public void PrintReport()
+        {
+            //this will show print dialog and call print methog of webbrowser control
+            DialogResult dr = printDialog1.ShowDialog();
+
+            if (dr.ToString() == "OK")
+            {
+                webBrowser1.Print();
+            }
+            else
+            {
+                return;
+            }
+
+        }
+    }
+}
